112292a830dcea06e9522ae02c2a73655fc3f268
JooJo <sjak0101@gmail.com>
2022-09-08 05:20:33 +0100

    implement form input

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVHil_toV5B0zp5JMFfKfE1s_0nqaXsps",
+  authDomain: "email-password-atuh.firebaseapp.com",
+  projectId: "email-password-atuh",
+  storageBucket: "email-password-atuh.appspot.com",
+  messagingSenderId: "539792019341",
+  appId: "1:539792019341:web:46124619e96f71edf777c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
